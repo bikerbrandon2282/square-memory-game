a20c5f47f84fa84f729cdf5fa4443535ef870a0b
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-07-15 15:34:09 +0100

    changing event listener to pointerdown from click event does not register on mobile, also removed previous touch-action in CSS as this did not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ function newGame() {
   game.playerMoves = [];
   for (let square of document.getElementsByClassName("square")) {
     if (square.getAttribute("data-listener") !== "true") {
-      square.addEventListener("click", (e) => {
+      square.addEventListener("pointerdown", (e) => {
         if(game.currentGame.length > 0 && !game.turnInProgress) {
           let move = e.target.getAttribute("id");
           game.lastButton = move;
